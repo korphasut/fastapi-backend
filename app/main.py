06ec1cbf05d8bf66f0e9717bb8e0b423859e0d98
Phasut Puasombatsak <phasutpuasombatsak@Phasuts-Laptop.local>
2025-02-16 01:58:12 +0700

    Allow * for now

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://new-frontend-55s.pages.dev/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
