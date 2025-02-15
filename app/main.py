c52f1dfb1844d3c96b31fa43d1c4a5c80acdbd33
Phasut Puasombatsak <phasutpuasombatsak@Phasuts-Laptop.local>
2025-02-16 00:37:21 +0700

    Fix CORS issue

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from app.api import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://new-frontend-55s.pages.dev/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 
